5e69e1e26bdb1468b934fb294ea6ef4a04b40d0b
BayKeremm <kerem13okyay@gmail.com>
2024-04-17 14:28:32 +0200

    AAA

--- apply_filters.sh
+++
@@ -1,6 +0,0 @@
-r1 sudo nft -f r1.nft &&
-r2 sudo nft -f r2.nft &&
-http sudo nft -f http.nft &&
-ntp sudo nft -f ntp.nft &&
-ftp sudo nft -f ftp.nft &&
-dns sudo nft -f dns.nft 
---
+++ attacks/network_scan.py
@@ -0,0 +1,9 @@
+'''
+Target subnets:
+    - 10.12.0.0/24
+    - 10.1.0.0/24
+
+First 24 bits (3 bytes) are fixed so we are only going to attack 8 bits > 2**8 * 2 ip addresses to test
+
+'''
+
